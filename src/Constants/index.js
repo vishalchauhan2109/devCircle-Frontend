e40be83e909c723352dc65071d2f9ddde8d713b0
vishalchauhan2109 <vishalchauhan2109@gmail.com>
2026-01-31 23:32:25 +0530

    Updated backend URL

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,5 +1,4 @@
 const isLocalhost = ["localhost", "127.0.0.1"].includes(window.location.hostname);
 
-export const baseUrl = isLocalhost
-  ? "http://localhost:2100"
-  : "https://be-devcircle.onrender.com";
+export const baseUrl = (!isLocalhost)? "https://dev-circle-5cxe-bz7h1czx1-vishalchauhan2109s-projects.vercel.app"  
+  :"http://localhost:2100";
